b4785b3d200023f2f3da7118272e9af09766ab14
Sohail Ahmad <181189626+sohail655@users.noreply.github.com>
2024-10-29 12:05:04 +0000

    dev container fig

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,15 @@
+const accountId = 123456
+let accountPassword = "11223344"
+var userEmail = "s@s.com"
+accountCity="jampur"
+
+
+//console.log(accountId);
+//console.table([accountId,accountPassword,accountCity,userEmail])
+
+//let accountId = 4 //cant change const
+console.log(accountId);
+
+
+
+
