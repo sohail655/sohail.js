bb4a724b899f0d861d0d7410898a4bea99fdef34
Sohail Ahmad <181189626+sohail655@users.noreply.github.com>
2024-10-29 12:35:42 +0000

    datatypes

---
+++ 01-basics/01-datatypes.js
@@ -0,0 +1,27 @@
+"use strict"  // treat all code in newer version
+
+//alert(3+3)  // keep care we are using node not browser consle
+
+console.log(3
+    +
+    3
+); //prefer code readability
+
+console.log('sohail')
+
+let name = "sohail"  
+let age = 19
+let isLoggedIn = false
+let state;
+
+//number = 2 the power of 53
+//big int
+//strings
+//objects
+//null
+//undefined
+//boolean
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
