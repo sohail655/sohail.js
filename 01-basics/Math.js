d52b9a110bec04e9d5f18c18a4ef87c7f42433c7
Sohail Ahmad <181189626+sohail655@users.noreply.github.com>
2024-10-30 04:54:16 +0000

    maths

---
+++ 01-basics/Math.js
@@ -0,0 +1,35 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toFixed(2)); // for fixed 
+// console.log(balance.toString().length); // for detection of length
+
+const newNumber = 1234.5678
+// console.log(newNumber.toPrecision(5));
+
+
+const hundreds = 10054356748
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-3)); // to convert in positve value
+console.log(Math.round(4.3));
+
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+
+
+
+
+
+
+
+
+
+
