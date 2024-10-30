f9b41c7ae56f5fc9bb99ed1a4e929b07a85c8aa1
Sohail Ahmad <181189626+sohail655@users.noreply.github.com>
2024-10-30 04:09:14 +0000

    changes

---
+++ 01-basics/01-strings.js
@@ -0,0 +1,26 @@
+const name = "sohail"
+const repoCount = " 50"
+
+//console.log(name+repoCount+"value");  // old syntax preffer to use new syntax
+
+console.log(`My name is ${name} and i have ${repoCount} repositries on github`); // it is good to use
+
+const channelName = new String("sohail-com-jampur")
+
+console.log(channelName.split("-")); // for trim anithing
+
+let account = "    sohail    "
+console.log(account);
+console.log(account.trim(""));  // trim white spaces
+
+console.log(account.replace("s", "MS")); // replace anything in stirngs
+
+console.log(account.indexOf("o"))
+console.log(account.charAt(5));  // its answer is o because of whitespaces
+
+
+
+
+
+
+ 
---
+++ 01-basics/conversion.js
@@ -0,0 +1,15 @@
+let score = "Sohail"
+
+//console.log(score);
+//console.log(typeof score);
+
+//let valueInNumber = Number(score)
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+
+//Notes
+// 33 => 33
+// sohail => NaN
+// true => 1 , false => 1
+
+
