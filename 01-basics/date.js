723951a3443a55400d59ec538581ed488c999074
Sohail Ahmad <181189626+sohail655@users.noreply.github.com>
2024-11-01 05:35:25 +0000

    date

---
+++ 01-basics/date.js
@@ -0,0 +1,21 @@
+let myDate = new Date()
+// console.log(myDate);
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2024, 11 , 01 , 10 , 30)
+// console.log(myCreatedDate);
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());  
+console.log(newDate.getMonth()+1);  
+console.log(newDate.getFullYear());  
+
+
+
