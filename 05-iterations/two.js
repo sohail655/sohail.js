70c325012abadb51adbb470644dd22c34996aed0
Sohail Ahmad <181189626+sohail655@users.noreply.github.com>
2024-11-02 06:33:01 +0000

    while and dowhile

---
+++ 05-iterations/two.js
@@ -0,0 +1,24 @@
+let  index = 0 
+while (index <= 10) {
+    // console.log(`The index value is ${index}`);
+    index ++ 
+}
+let  i = 0 
+while (i <= 10) {
+    // console.log(`The index value is ${i}`);
+    i=i+2
+}
+
+let myArray = [ "sohail", "Alki", "asad"]
+let arr=0
+while (arr < myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+     arr++
+    
+}
+
+let score =11
+do {
+    console.log(`value is ${score}`);
+    score++
+} while (score<=10);
